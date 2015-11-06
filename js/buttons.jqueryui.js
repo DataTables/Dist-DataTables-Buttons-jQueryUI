0706d6c61ae5c206ddcece880ea6958aee711ef8
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-11-06 07:36:14 +0000

    Dev fix: Column reordering could cause columns which weren't in the list of columns for column visibility to be shown.
    - This fix addresses that, but it also results in the buttons being
      shown in their original order rather than in the new order. That will
      need to be addressed with another commit
    
    Sync to source repo @325a071b834a71479e15a721dcbae7800fbc1a5e

--- js/buttons.jqueryui.js
+++ js/buttons.jqueryui.js
@@ -1,7 +1,32 @@
+/*! jQuery UI integration for DataTables' Buttons
+ * ©2015 SpryMedia Ltd - datatables.net/license
+ */
 
-(function($, DataTables){
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-jqui', 'datatables.net-buttons'], factory );
+	}
+	else if ( typeof exports === 'object' ) {
+		// Node / CommonJS
+		module.exports = function ($, dt) {
+			if ( ! $ ) { $ = require('jquery'); }
+			if ( ! $.fn.dataTable ) { require('datatables.net-jqui')($); }
+			if ( ! $.fn.dataTable.Buttons ) { require('datatables.net-buttons')($); }
 
-$.extend( true, DataTables.Buttons.defaults, {
+			factory( $ );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery );
+	}
+}(function( $ ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
+$.extend( true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
 			className: 'dt-buttons ui-buttonset'
@@ -18,9 +43,10 @@ $.extend( true, DataTables.Buttons.defaults, {
 	}
 } );
 
-DataTables.ext.buttons.collection.text = function ( dt ) {
+DataTable.ext.buttons.collection.text = function ( dt ) {
 	return dt.i18n('buttons.collection', 'Collection <span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"/>');
 };
 
 
-})(jQuery, jQuery.fn.dataTable);
+return DataTable.Buttons;
+}));
